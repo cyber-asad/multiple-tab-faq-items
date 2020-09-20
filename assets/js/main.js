6fb014cb0455bb834c550e9bb3b9eddb99df5f51
Cyber Asad <cyber.asad24@gmail.com>
2020-09-20 21:23:40 +0600

    Added functionality to the tab and faq items

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,29 +4,29 @@
     jQuery(document).ready(function ($) {
 
 
-        // faqtab Mechanism Start
-        var $faqtabSelectors = $(".faq-tab-button"); // faqtab Selectors
+        // FaqTab Mechanism Start
+        var $faqTabSelectors = $(".faq-tab-button"); // FaqTab Selectors
 
-        // Looping through each faqtab selectors
-        $faqtabSelectors.each(function () {
+        // Looping through each FaqTab selectors
+        $faqTabSelectors.each(function () {
 
-            var $selectorSelf = $(this);    // faqtab current selector
+            var $selectorSelf = $(this);    // FaqTab current selector
             var $selectorNumber = $selectorSelf.data('faqtab');    // Current selector's data number
 
             // Handler function based on click
             $selectorSelf.click(function () {
 
-                // Add/Remove Active Class from faqtab Selectors
+                // Add/Remove Active Class from FaqTab Selectors
                 var $siblingSelectors = $(this).siblings(".faq-tab-button");
                 $siblingSelectors.removeClass("active");
                 $(this).addClass("active");
 
                 var $parentWrapper = $selectorSelf.closest(".faq-tab-wrapper"); // Parent wrapper
-                var $faqtabItems = $parentWrapper.find(".faq-tab-item");       // faqtab items
+                var $faqtabItems = $parentWrapper.find(".faq-tab-item");       // FaqTab items
 
-                // Looping through each faqtab items inside the parent wrapper
+                // Looping through each FaqTab items inside the parent wrapper
                 $faqtabItems.each(function () {
-                    var $itemSelf = $(this);    // faqtab item
+                    var $itemSelf = $(this);    // FaqTab item
                     var $itemNumber = $itemSelf.data('faqtab');    // Item's data number
 
                     // Hide/Show items based on data numbers
@@ -43,6 +43,30 @@
 
         });
         // faqtab Mechanism End
+
+
+
+
+
+        var $faqTitles = $(".faq-title"); // FaqTab Selectors
+
+        $faqTitles.each(function () {
+            var $titleSelf = $(this);
+
+            $titleSelf.click(function() {
+                var $currentFaqItem = $(this).closest(".faq-single");
+                var $currentFaqContent = $currentFaqItem.find(".faq-content");
+                
+                var $parentFaqColumn = $currentFaqItem.closest(".faq-column");
+                var $allFaqContents = $parentFaqColumn.find(".faq-content");
+
+                $allFaqContents.slideUp();
+                $currentFaqContent.slideDown();
+
+            });
+
+        });
+
 
 
 
